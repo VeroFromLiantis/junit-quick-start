24e1ad8a3bd810837d6fbf4d00a4cf49badf49be
Veronique De Bisscop <veronique.debisscop@liantis.be>
2019-02-20 12:19:55 +0100

    met mockito extensions

--- src/test/java/Vero/GreetingServiceTest.java
+++ src/test/java/Vero/GreetingServiceTest.java
@@ -2,6 +2,38 @@ package Vero;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+
+public class GreetingServiceTest {
+	
+	@Mock 
+	private Hello helloMock;
+	@InjectMocks
+    private GreetingService greeting;
+	
+	@Test
+	public void testGreet() {
+		// prepare mock
+		Mockito.when(helloMock.sayHello("World"))
+	           .thenReturn("Hello World");
+		// execute test
+		String answer = greeting.greet("World");
+		assertEquals("Greeting message: Hello World", answer);
+	}
+
+}
+
+
+/* werkwijze zonder Mockito Extension 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -9,9 +41,10 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import org.mockito.Mockito;
 
-@TestInstance(Lifecycle.PER_CLASS)
+ @TestInstance(Lifecycle.PER_CLASS)
 
 public class GreetingServiceTest {
+	
 	private GreetingService greeting;
 	private Hello helloMock;
 			
@@ -32,3 +65,4 @@ public class GreetingServiceTest {
 	}
 
 }
+*/
